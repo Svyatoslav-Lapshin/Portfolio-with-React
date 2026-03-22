e501502ce1730849b44cf63be140cdf94993090c
Svyatoslav <slavalapshin17@gmail.com>
2026-03-22 07:19:36 +0100

    removed empty spaces and added an icon

--- Portfolio/src/main.jsx
+++ Portfolio/src/main.jsx
@@ -1,8 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
 import App from "./App.jsx";
-import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
